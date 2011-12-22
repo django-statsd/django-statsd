76131e0a73c5b9d44dd7a6327aa5b3d9bbe03048
Andy McKay <amckay@mozilla.com>
2011-12-22 14:34:53 -0800

    faster host lookup

--- django_statsd/clients/__init__.py
+++ django_statsd/clients/__init__.py
@@ -1,3 +1,5 @@
+import socket
+
 from django.utils.importlib import import_module
 from django.conf import settings
 
@@ -5,9 +7,9 @@ _statsd = None
 
 
 def get_client():
-    print 'getting client'
     client = getattr(settings, 'STATSD_CLIENT', 'statsd.client')
     host = getattr(settings, 'STATSD_HOST', 'localhost')
+    host = socket.gethostbyaddr(host)[2][0]
     port = getattr(settings, 'STATSD_PORT', 8125)
     prefix = getattr(settings, 'STATSD_PREFIX', None)
     return import_module(client).StatsClient(host, port, prefix)
