9dd486a2fe5cbdbcd80a479984a7008d2229c879
Andy McKay <amckay@mozilla.com>
2012-06-07 15:52:00 -0700

    remove lookup for the moment

--- django_statsd/clients/__init__.py
+++ django_statsd/clients/__init__.py
@@ -9,7 +9,10 @@ _statsd = None
 def get_client():
     client = getattr(settings, 'STATSD_CLIENT', 'statsd.client')
     host = getattr(settings, 'STATSD_HOST', 'localhost')
-    host = socket.gethostbyaddr(host)[2][0]
+# This is causing problems with statsd
+# gaierror ([Errno -9] Address family for hostname not supported)
+# TODO: figure out what to do here.
+#    host = socket.gethostbyaddr(host)[2][0]
     port = getattr(settings, 'STATSD_PORT', 8125)
     prefix = getattr(settings, 'STATSD_PREFIX', None)
     return import_module(client).StatsClient(host, port, prefix)
