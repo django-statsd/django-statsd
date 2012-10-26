43a8e95fe3839c24b1dff39b75b413436b42b5e8
Andy McKay <amckay@mozilla.com>
2012-10-26 15:10:20 -0700

    cope with non tastypie urls

--- django_statsd/middleware.py
+++ django_statsd/middleware.py
@@ -52,6 +52,9 @@ class TastyPieRequestTimingMiddleware(GraphiteRequestTimingMiddleware):
     """statd's timing specific to Tastypie."""
 
     def process_view(self, request, view_func, view_args, view_kwargs):
-        request._view_module = view_kwargs['api_name']
-        request._view_name = view_kwargs['resource_name']
-        request._start_time = time.time()
+        try:
+            request._view_module = view_kwargs['api_name']
+            request._view_name = view_kwargs['resource_name']
+            request._start_time = time.time()
+        except AttributeError:
+            pass
