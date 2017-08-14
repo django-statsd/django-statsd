8cde8a082196d446c75903e0f8d6a23edc166c7f
Michael K <michael-k@users.noreply.github.com>
2017-08-14 13:52:47 +0200

    Removed compatibility code for django < 1.6

--- django_statsd/patches/db.py
+++ django_statsd/patches/db.py
@@ -1,4 +1,3 @@
-import django
 try:
     from django.db.backends import utils as util
 except ImportError:
@@ -10,22 +9,6 @@ from django_statsd.clients import statsd
 
 def key(db, attr):
     return 'db.%s.%s.%s' % (db.client.executable_name, db.alias, attr)
-
-
-def pre_django_1_6_cursorwrapper_getattr(self, attr):
-    """
-    The CursorWrapper is a pretty small wrapper around the cursor.
-    If you are NOT in debug mode, this is the wrapper that's used.
-    Sadly if it's in debug mode, we get a different wrapper.
-    """
-    if self.db.is_managed():
-        self.db.set_dirty()
-    if attr in self.__dict__:
-        return self.__dict__[attr]
-    else:
-        if attr in ['execute', 'executemany', 'callproc']:
-            return wrap(getattr(self.cursor, attr), key(self.db, attr))
-        return getattr(self.cursor, attr)
 
 
 def _get_query_type(query):
@@ -50,20 +33,8 @@ def patched_callproc(orig_callproc, self, query, *args, **kwargs):
 def patch():
     """
     The CursorWrapper is a pretty small wrapper around the cursor.  If
-    you are NOT in debug mode, this is the wrapper that's used.  Sadly
-    if it's in debug mode, we get a different wrapper for version
-    earlier than 1.6.
+    you are NOT in debug mode, this is the wrapper that's used.
     """
-
-    if django.VERSION > (1, 6):
-        # In 1.6+ util.CursorDebugWrapper just makes calls to CursorWrapper
-        # As such, we only need to instrument CursorWrapper.
-        # Instrumenting both will result in duplicated metrics
-        patch_method(util.CursorWrapper, 'execute')(patched_execute)
-        patch_method(util.CursorWrapper, 'executemany')(patched_executemany)
-        patch_method(util.CursorWrapper, 'callproc')(patched_callproc)
-    else:
-        util.CursorWrapper.__getattr__ = pre_django_1_6_cursorwrapper_getattr
-        patch_method(util.CursorDebugWrapper, 'execute')(patched_execute)
-        patch_method(
-            util.CursorDebugWrapper, 'executemany')(patched_executemany)
+    patch_method(util.CursorWrapper, 'execute')(patched_execute)
+    patch_method(util.CursorWrapper, 'executemany')(patched_executemany)
+    patch_method(util.CursorWrapper, 'callproc')(patched_callproc)
